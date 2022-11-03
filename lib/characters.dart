83ed63a301c16cd5ccc146edbea6482941060b51
Florian <florianlaignez@gmail.com>
2022-11-03 14:51:57 +0100

    Ajout scroll infini

--- lib/characters.dart
+++ lib/characters.dart
@@ -15,18 +15,36 @@ class CharacterPage extends StatefulWidget {
 
 class _CharacterPageState extends State<CharacterPage> {
   List<dynamic> characters = [];
+  int _currentLoadedPage = 1;
+  final ScrollController _scrollController = ScrollController();
   @override
   void initState() {
     super.initState();
-    fetchData();
-  }
-  void fetchData() async {
-    final result = await http.get(Uri.parse('https://rickandmortyapi.com/api/character'));
-    final json = jsonDecode(result.body);
-    final List<dynamic> jsonCharacters = json['results'];
-    setState(() {
-      characters = jsonCharacters;
+    fetchData(_currentLoadedPage);
+    _scrollController.addListener(() {
+      if (_scrollController.position.extentAfter < 500) {
+        _currentLoadedPage++;
+        fetchData(_currentLoadedPage);
+      }
     });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+
+  void fetchData(int pageNumber) async {
+    final result = await http.get(Uri.parse('https://rickandmortyapi.com/api/character?page=$pageNumber'));
+    if(result.statusCode == 200){
+      final json = jsonDecode(result.body);
+      final List<dynamic> jsonCharacters = json['results'];
+      setState(() {
+        characters.addAll(jsonCharacters);
+      });
+    }
   }
 
   @override
@@ -35,9 +53,11 @@ class _CharacterPageState extends State<CharacterPage> {
     if(characters.isEmpty){
       content = const Center(child: CircularProgressIndicator());
     }else{
-      content = ListView(
-        children: 
-          characters.map((character) => InkWell(
+      content = ListView.builder(
+        controller: _scrollController,
+        itemCount: characters.length,
+        itemBuilder: (BuildContext context, int index) { 
+          dynamic character = characters[index];return InkWell(
             onTap: () => Navigator.of(context).push(MaterialPageRoute(
               builder: (context) => CharacterDetailsPage(id: character['id'], imageUrl: character['image'])
             )),
@@ -45,7 +65,8 @@ class _CharacterPageState extends State<CharacterPage> {
               imageUrl: character['image'],
               name: character['name']
             ),
-          )).toList()
+        );
+        },
         
       );
     }
